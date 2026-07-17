6b3d3a5ba749b5c53ee498a67d4f4787f1640bbf
浪子 <jkjoy@live.cn>
2026-07-17 09:41:06 +0800

    Bump theme version to 1.4.6

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
  * 原主题为Wordpress主题 原作者 huitheme 
  * @package  Once 
  * @author 老孙
- * @version 1.4.5
+ * @version 1.4.6
  * @link http://www.imsun.org
  */
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
